c7932efac483e35b98923e0d4817043c9d4e4af8
tanmay30 <tanmay@synctactic.ai>
2020-04-19 21:04:25 +0530

    added zodb support

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import ZODB
 from src import app
+
+db = ZODB(app)
+
 if __name__ == '__main__':
     app.run()
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,13 @@
 from flask import Blueprint
+from app import db
 
 api_routes = Blueprint('account_api', __name__)
+
+@app.before_request
+def set_db_defaults():
+    if 'entries' not in db:
+        db['entries'] = List()
+
 
 @api_routes.route("/")
 def hello():
